8153572f97179100a85fcaebba5cc5c296307fd7
Dina Polousova <dinapolousova@gmail.com>
2024-01-22 17:52:03 +0300

    basic html structure

--- src/App.js
+++ src/App.js
@@ -4,9 +4,47 @@ import './App.css';
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Technologies />
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src="./logo.jpeg" alt="logo"></img>
+      </header>
+
+      <nav className='nav'>
+        <div><a>Profile</a></div>
+        <div><a>Messages</a></div>
+        <div><a>News</a></div>
+        <div><a>Music</a></div>
+        <div><a>Settings</a></div>
+
+      </nav>
+
+      <div className='content'>
+        <div>
+          <img src="https://ya.ru/images/search?source=serp&text=картинка+растущие+овощи&pos=17&rpt=simage&img_url=https%3A%2F%2Fi.sunhome.ru%2Fjournal%2F112%2Fvegetarianstvo-vhodit-v-modu-v2.orig.jpg&lr=10748" alt="main photo"></img>
+        </div>
+
+        <div>
+          ava + description
+        </div>
+
+        <div>
+          My posts
+          <div>
+            New post
+          </div>
+            <div>
+              post 1
+            </div>
+            <div>
+              post 2
+            </div>
+          <div>
+
+          </div>
+        </div>
+
+      </div>
+
     </div>
 
   );
