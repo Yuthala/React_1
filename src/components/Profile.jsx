ea6cf8804b14ce2249138b3d3e9d615dad74aa61
Dina Polousova <dinapolousova@gmail.com>
2024-01-27 11:36:13 +0300

    Profile.module.css

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,8 +1,9 @@
-import './Profile.css';
+
+import classes from './Profile.module.css';
 
 const Profile = () => {
 	return (
-		<div className='content'>
+		<div className={classes.content}>
 			<div>
 				<img src="https://ya.ru/images/search?source=serp&text=картинка+растущие+овощи&pos=17&rpt=simage&img_url=https%3A%2F%2Fi.sunhome.ru%2Fjournal%2F112%2Fvegetarianstvo-vhodit-v-modu-v2.orig.jpg&lr=10748" alt="main photo"></img>
 			</div>
@@ -16,11 +17,11 @@ const Profile = () => {
 				<div>
 					New post
 				</div>
-				<div className="posts">
-					<div className="item">
+				<div className={classes.posts}>
+					<div className={classes.item}>
 						post 1
 					</div>
-					<div className="item">
+					<div className={classes.item}>
 						post 2
 					</div>
 				</div>
