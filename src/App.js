5b7ae617968f2dff6164b68237cf0b868dff3052
Dina Polousova <dinapolousova@gmail.com>
2024-01-31 14:06:07 +0300

    <Router /> component

--- src/App.js
+++ src/App.js
@@ -2,21 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header.jsx';
 import Navbar from './components/Navbar/Navbar.jsx';
-//import Profile from './components/Profile/Profile.jsx';
+import Profile from './components/Profile/Profile.jsx';
 import Dialogs from './components/Dialogs/Dialogs.jsx';
-
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className='app-wrapper'>
-      <Header />
-      <Navbar />
-      {/* <Profile /> */}
-      <div className='app-wrapper-content'>
-        <Dialogs />
+    <BrowserRouter>
+        <div className='app-wrapper'>
+          <Header />
+          <Navbar />
+          {/* <Profile /> */}
+          <div className='app-wrapper-content'>
+            <Routes>
+              <Route path='/dialogs' component={Dialogs}/>
+              <Route path='/profile' component={Profile}/>
+            </Routes>
+          </div>
       </div>
+    </BrowserRouter>
 
-    </div>
 
   );
 };
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,12 @@
 
 import classes from './Navbar.module.css';
 
-// const classes = {
-// 	'nav': 'Navbar_nav__DS0RX',
-// 	'item': 'Navbar_nav__DS0RX'
-// }
 
 const Navbar = () => {
 	return (
 		<nav className={classes.nav}>
-			<div className={`${classes.item} ${classes.active}`}><a>Profile</a></div>
-			<div className={classes.item}><a>Messages</a></div>
+			<div className={`${classes.item} ${classes.active}`}><a href="/profile">Profile</a></div>
+			<div className={classes.item}><a href="/dialogs">Messages</a></div>
 			<div className={classes.item}><a>News</a></div>
 			<div className={classes.item}><a>Music</a></div>
 			<div className={classes.item}><a>Settings</a></div>
