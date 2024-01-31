c83251a5671e4d77b7cd6757fbb9151a22615b4b
Dina Polousova <dinapolousova@gmail.com>
2024-01-31 13:02:37 +0300

    Dialogs profile

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header.jsx';
 import Navbar from './components/Navbar/Navbar.jsx';
-import Profile from './components/Profile/Profile.jsx';
+//import Profile from './components/Profile/Profile.jsx';
+import Dialogs from './components/Dialogs/Dialogs.jsx';
 
 
 const App = () => {
@@ -10,7 +11,11 @@ const App = () => {
     <div className='app-wrapper'>
       <Header />
       <Navbar />
-      <Profile />
+      {/* <Profile /> */}
+      <div className='app-wrapper-content'>
+        <Dialogs />
+      </div>
+
     </div>
 
   );
