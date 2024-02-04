0386dc1d3db361c37067b15075c68ae1b321e1c3
Dina Polousova <dinapolousova@gmail.com>
2024-02-05 00:23:09 +0300

    replace hardcode with arrays in MyPosts.jsx

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,12 @@ import classes from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = () => {
+
+	let postsData = [
+		{id: 0, message: 'Hi, how are you?', likesCount: 12},
+		{id: 1, message: 'This is my first post!', likesCount: 11}
+	];
+
 	return (
 		<div className={classes.content}>
 
@@ -15,8 +21,8 @@ const MyPosts = () => {
 					<button>Add Post</button>
 				</div>
 				<div className={classes.posts}>
-					<Post message= 'Hi, how are you?'/>
-					<Post message= "It's my first post" />
+					<Post message= {postsData[0].message} likesCount = {postsData[0].likesCount} />
+					<Post message= {postsData[1].message} likesCount = {postsData[1].likesCount} />
 					<Post />
 				</div>
 			</div>
