3f205abf019e5378b09cb0665c504536fae098fa
Dina Polousova <dinapolousova@gmail.com>
2024-02-14 18:52:55 +0300

    React.createRef()

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,4 +1,4 @@
-
+import React from 'react';
 import classes from './MyPosts.module.css';
 import Post from './Post/Post';
 
@@ -13,16 +13,23 @@ const MyPosts = (props) => {
 let PostsElements = props.posts
 	.map (p => <Post message={p.message} likesCount={p.likesCount} id={p.id} />);
 
+	let newPostElement = React.createRef();
+
+	let addPost = () => {
+		let text = newPostElement.current.value;
+		alert('wikipedia.org');
+	}
+
 	return (
 		<div className={classes.content}>
 
 			<div className={classes.postsBlock}>
 				<h3>My posts</h3>
 				<div>
-					<textarea></textarea>
+					<textarea ref={newPostElement}></textarea>
 				</div>
 				<div>
-					<button>Add Post</button>
+					<button onClick={ addPost }>Add Post</button>
 				</div>
 				<div className={classes.posts}>
 					{ PostsElements}
