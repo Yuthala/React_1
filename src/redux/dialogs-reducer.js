32466d5ec255fca76c2128f6ba33a25809dfd5ff
Dina Polousova <dinapolousova@gmail.com>
2024-02-27 10:31:03 +0300

    Merge branch 'test'

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -2,15 +2,18 @@ const UPDATE_NEW_MESSAGE_BODY = 'UPDATE-NEW-MESSAGE-BODY';
 const SEND_MESSAGE = 'SEND-MESSAGE';
 
 const dialogsReducer = (state, action) => {
-	if (action.type === UPDATE_NEW_MESSAGE_BODY) {
-		state.newMessageBody = action.body;
-	} else if (action.type === SEND_MESSAGE) {
-		let body = state.newMessageBody;
-		state.newMessageBody = '';
-		state.messagesData.push({id: 6, message: body});
-	}	
-
-	return state;
-}
+	switch(action.type) {
+		case UPDATE_NEW_MESSAGE_BODY: 
+			state.newMessageBody = action.body;
+			return state;
+		case SEND_MESSAGE: 
+			let body = state.newMessageBody;
+			state.newMessageBody = '';
+			state.messagesData.push({id: 6, message: body});
+			return state;
+		default:
+			return state;
+	}
+};
 
 export default dialogsReducer;
